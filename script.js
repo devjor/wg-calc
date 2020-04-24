e8cb38ada603a0a943f941b4ac85ee99e7bfe1f5
devjor <49990748+devjor@users.noreply.github.com>
2020-04-24 12:39:08 +0100

    Reverse Test Case

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 const basicPay = 2013.08;
-const hourlyRate = 11.4977;
+const hourlyRate = 12.3543;
 const mileageRate = 0.56;
 const taxCode = 13890;
 const NiPrimaryThreshold = 792;
-const parking = 0;
+const parking = 12.5;
 
 var satHoursField = document.querySelector('.satHoursField');
 var sunHoursField = document.querySelector('.sunHoursField');
